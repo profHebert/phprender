540d6de766ceabb7b2545d5c8684f5ab0d4b382a
Hebert Bratefixe Alquimim <hebert@uni9.pro.br>
2026-09-20 12:10:07 -0300

    Dynamic header with PHP variable for name

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Aulas de PHP</title>
+</head>
+<body>
+  <?php
+  $nome="Render";
+  ?>
+    <h1>Aulas de PHP com <?php=$nome;?> </h1>
+    <p>Selecione um arquivo abaixo ou acesse diretamente pela URL:</p>
+    <ul>
+        <li><a href="/aula1.php">aula1.php - Variáveis</a></li>
+        <li><a href="/aula2.php">aula2.php - Estruturas Condicionais</a></li>
+    </ul>
+    </body>
+</html>
